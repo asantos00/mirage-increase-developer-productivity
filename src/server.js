43a0f3a03ad2584367c7f31719afc3776913611d
Alexandre Santos <alexandre.santozz@gmail.com>
2019-09-11 17:27:59 +0100

    Send dependencies to server
    - Add seeds method
    - Add routes method

--- src/server.js
+++ src/server.js
@@ -2,30 +2,36 @@ import { Server } from "@miragejs/server";
 
 const server = new Server({
   urlPrefix: "https://alexandrempsantos.com/",
-  namespace: "api"
-});
-
-server.get("/posts", () => [
-  {
-    id: 1,
-    title: "Mocking an API with axios",
-    author: "asantos00",
-    createdAt: 1557937282,
-    body: "Lorem ipsum dolor sit amet, consectetur."
+  namespace: "api",
+  seeds({ db }) {
+    db.loadData({
+      posts: [
+        {
+          id: 1,
+          title: "Mocking an API with axios",
+          author: "asantos00",
+          createdAt: 1557937282,
+          body: "Lorem ipsum dolor sit amet, consectetur."
+        },
+        {
+          id: 2,
+          title: "Forget axios interceptors. @miragejs/server",
+          author: "asantos00",
+          createdAt: 758851200,
+          body: "Lorem ipsum dolor sit amet, consectetur."
+        }
+      ]
+    });
   },
-  {
-    id: 2,
-    title: "Forget axios interceptors. @miragejs/server",
-    author: "asantos00",
-    createdAt: 758851200,
-    body: "Lorem ipsum dolor sit amet, consectetur."
-  }
-]);
+  routes() {
+    this.get("/posts", schema => schema.db.posts);
 
-server.put("/posts/:id", (schema, request) => {
-  schema.db.posts.update(request.params.id, {
-    title: request.requestBody.title
-  });
+    this.put("/posts/:id", (schema, request) => {
+      schema.db.posts.update(request.params.id, {
+        title: request.requestBody.title
+      });
+    });
+  }
 });
 
 server.passthrough();
